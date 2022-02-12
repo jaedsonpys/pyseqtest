5bbdd3d854c9ea0d359d74a3be9d7ddfce717b42
Jaedson Silva <contacanal798@gmail.com>
2022-02-12 18:46:26 -0300

    adicionando configuração em setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pyseqtest',
+    version='1.0.0',
+    description='Teste unitário sequencial',
+    py_modules=['pyseqtest'],
+    author='jaedsonpys',
+    author_email='imunknowuser@protonmail.com'
+)
