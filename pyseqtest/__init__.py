173d4de5774bbf598187eb41b7e013b8bee8b89a
Jaedson Silva <contacanal798@gmail.com>
2022-02-12 15:56:15 -0300

    adicionando docstring em __init__.py

--- pyseqtest/__init__.py
+++ pyseqtest/__init__.py
@@ -1,3 +1,10 @@
+"""
+PySeqTest foi criado para relizar testes
+unitários em sequência. Útil para testar
+aplicações que envolvem I/O, banco de dados
+e outros.
+"""
+
 from .main import SeqTest
 
 version = ('1.0.0', 'dev')
