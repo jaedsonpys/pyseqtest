1b18123fdfa9eb0196edbafb9b9d25b11516563e
Jaedson Silva <contacanal798@gmail.com>
2022-02-12 19:26:48 -0300

    adicionando long_description em setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open('README.rst', 'r') as readme:
+    readme_text = readme.read()
+
 setup(
     name='pyseqtest',
-    version='1.0.0',
+    version='1.0.1',
     description='Teste unitário sequencial',
     py_modules=['pyseqtest'],
     author='jaedsonpys',
-    author_email='imunknowuser@protonmail.com'
+    author_email='imunknowuser@protonmail.com',
+    long_description=readme_text,
+    long_description_content_type='text/x-rst'
 )
